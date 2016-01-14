1ffc9d857829ac0242b005b28ed8fc745193d523
Tyler Graham <tylergraham134@gmail.com>
2016-01-13 23:20:16 -0700

    update port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
