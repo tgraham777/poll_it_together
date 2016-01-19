86308c48d6392f4e870b0fe2ef677ebcd854cbe4
Tyler Graham <tylergraham134@gmail.com>
2016-01-19 09:01:28 -0700

    refactors pollCreator test to reduce redundancies

--- test/pollCreator_test.js
+++ test/pollCreator_test.js
@@ -6,6 +6,15 @@ const PollCreator = require('../pollCreator');
 const Poll = require('../poll');
 
 var pollCreator = new PollCreator();
+var pollParams = { pollName: 'test',
+  pollDescription: 'test',
+  questions: {
+    question1: 'apple',
+    question2: 'banana'
+  },
+  pollEnd: '08/22/2016 8:16 AM',
+  showPollResults: 'Yes'
+}
 
 describe('PollCreator', function () {
   it('has a "polls" attribute that starts as an empty hash', function(done) {
@@ -14,45 +23,18 @@ describe('PollCreator', function () {
   });
 
   it('can create and save a poll and find the poll by its id', function(done) {
-    var pollParams = { pollName: 'test',
-      pollDescription: 'test',
-      questions: {
-        question1: 'apple',
-        question2: 'banana'
-      },
-      pollEnd: '08/22/2016 8:16 AM',
-      showPollResults: 'Yes'
-    }
     var poll = pollCreator.create(pollParams);
     expect(pollCreator.findPollById(poll.poll_id)).eql(poll);
     done();
   });
 
   it('can find a saved poll by the admin id', function(done) {
-    var pollParams = { pollName: 'test',
-      pollDescription: 'test',
-      questions: {
-        question1: 'apple',
-        question2: 'banana'
-      },
-      pollEnd: '08/22/2016 8:16 AM',
-      showPollResults: 'Yes'
-    }
     var poll = pollCreator.create(pollParams);
     expect(pollCreator.findPollByAdminId(poll.admin_id)).eql(poll);
     done();
   });
 
   it('can find a saved poll by the links id', function(done) {
-    var pollParams = { pollName: 'test',
-      pollDescription: 'test',
-      questions: {
-        question1: 'apple',
-        question2: 'banana'
-      },
-      pollEnd: '08/22/2016 8:16 AM',
-      showPollResults: 'Yes'
-    }
     var poll = pollCreator.create(pollParams);
     expect(pollCreator.findPollByLinksId(poll.links_id)).eql(poll);
     done();
